56016d60a1b9d439f71e31d9fa69df0937f2d7f6
Vsevolod Novikov <nnseva@gmail.com>
2017-11-01 20:43:10 +0300

    Fixed version to distribute
    [skip ci]

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.1b1',
+    version='0.0.1b2',
 
     description='Django-Access - the application introducing dynamic evaluation-based instance-level (row-level) access rights control for Django',
     long_description=long_description,
