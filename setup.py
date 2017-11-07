cb4961fe8a4af279db12956f39abffc2df370ee2
Vsevolod Novikov <nnseva@gmail.com>
2017-11-07 13:37:19 +0300

    Fixed build tools

--- setup.py
+++ setup.py
@@ -106,12 +106,14 @@ setup(
     #package_data={
     #    'sample': ['package_data.dat'],
     #},
-
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
     #data_files=[('my_data', ['data/data_file'])],
+    data_files=[
+        ('', ['VERSION']),
+    ]
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
