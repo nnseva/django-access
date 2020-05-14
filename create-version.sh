c7c28b0eeb52023e89b39dab63c79bd9fdbeca23
Vsevolod Novikov <nnseva@gmail.com>
2020-05-14 17:57:07 +0300

    Fixed script

--- create-version.sh
+++ create-version.sh
@@ -7,7 +7,7 @@ desc=$*
 echo "VERSION:" $version >&2
 echo __version__ = \"$version\" >access/__init__.py
 
-git commit -a -m "Fixed version to $version"
+git commit -a -m "$*"
 git tag $version -a -m "$*"
 
 exit 0
